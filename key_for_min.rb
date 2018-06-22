6fa6c22927253cf9681e9844698e739ec9dd35b0
Vicki Hamm <vicki.cello@gmail.com>
2018-06-22 15:41:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   name_hash.collect do | key, value |
     if min_value == nil || min_value > value
-    
+
       min_value = value
       min_key = key
     end
