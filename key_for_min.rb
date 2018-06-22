002c5bc6a954c2437217e6cf047ec592022bf347
Vicki Hamm <vicki.cello@gmail.com>
2018-06-22 15:37:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,11 @@
 def key_for_min_value(name_hash)
   min_value = 0
   k_for_min_value = nil
-  
+
   name_hash.each_pair do | key, value |
     if value > min_value
       min_value = value
       k_for_min_value = key
     end
+  end
 end
-
